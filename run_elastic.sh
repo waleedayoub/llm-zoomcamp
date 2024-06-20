0c2370d21651f9e18325b1bd216b44b1b2d97c25
waleed <waleed.ayoub@gmail.com>
2024-06-20 21:55:30 +0000

    three lectures in and environment is set up

---
+++ run_elastic.sh
@@ -0,0 +1,8 @@
+docker run -it \
+    --rm \
+    --name elasticsearch \
+    -p 9200:9200 \
+    -p 9300:9300 \
+    -e "discovery.type=single-node" \
+    -e "xpack.security.enabled=false" \
+    docker.elastic.co/elasticsearch/elasticsearch:8.4.3
